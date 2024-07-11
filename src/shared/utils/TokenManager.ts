63e4dcced82d0ac9499e5b1ab0f9bcdb2038d458
Josuesp1620 <josuesp1620@gmail.com>
2024-07-10 20:21:02 -0500

    Update Util JWT Token

--- src/shared/utils/TokenManager.ts
+++ src/shared/utils/TokenManager.ts
@@ -1,45 +1,45 @@
-import jwt from 'jsonwebtoken'
-import ms from 'ms'
+import jwt from 'jsonwebtoken';
+import ms from 'ms';
 
 export class TokenManager {
-    private readonly secretKey: string
+    private readonly secretKey: string;
 
     constructor() {
-        this.secretKey = process.env.JWT_SECRET_KEY || 'jwt_secret_key'
+        this.secretKey = process.env.JWT_SECRET_KEY || 'jwt_secret_key';
     }
 
     public generateAccessToken(payload: any, role: string): string {        
         return jwt.sign(
-            {id: payload, role, type: 'access'},        
+            { id: payload, role, type: 'access' },        
             this.secretKey,
-            { expiresIn: ms(3600000) }
+            { algorithm: 'HS256', expiresIn: ms('1h') }
         );
     }
 
     public generateRefreshToken(payload: any, role: string): string {        
         return jwt.sign(
-            {id: payload, role, type: 'refresh'},
+            { id: payload, role, type: 'refresh' },
             this.secretKey,
-            { expiresIn: ms(3600000 * 24) }
+            { algorithm: 'HS256', expiresIn: ms('24h') }
         );
     }
     
     public verifyToken(token: string): { expired: boolean, decoded: any | null } {
         try {
-            const decoded = jwt.verify(token, this.secretKey, { complete: true }) as { [key: string]: any }
-            const { email, role, user_id, username, type } = decoded.payload
-            return { expired: false, decoded: { email, role, user_id, username, type } }
+            const decoded = jwt.verify(token, this.secretKey, { algorithms: ['HS256'] }) as { [key: string]: any };
+            const { role, type } = decoded;
+            return { expired: false, decoded: { role, type } };
         } catch (error) {
-            return { expired: true, decoded: null }
+            return { expired: true, decoded: null };
         }
     }
 
     public decodeToken(token: string): any | null {
         try {
-            const decoded = jwt.decode(token) as any
-            return decoded
+            const decoded = jwt.decode(token) as any;
+            return decoded;
         } catch (error) {
-            return null
+            return null;
         }
     }
 }
