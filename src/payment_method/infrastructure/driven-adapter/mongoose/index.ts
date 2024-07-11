da583f66f0c4c4ddf93cef72137d4105e53fdb6c
Josuesp1620 <josuesp1620@gmail.com>
2024-07-11 14:12:39 -0500

    [Fix] Update Payment Method Mongoose Schema

--- src/payment_method/infrastructure/driven-adapter/mongoose/index.ts
+++ src/payment_method/infrastructure/driven-adapter/mongoose/index.ts
@@ -1,38 +1,11 @@
 import { mongoose as conector_mongoose } from '@/shared/services/mongo-connector'
 
 const PaymentMethodSchema = new conector_mongoose.Schema({
-    first_name: {
+    name: {
         type: String,
         required: true,
     },
-    last_name: {
-        type: String,
-        required: true,
-    },
-    phone_number: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    street: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    country: {
-        type: String,        
-    },
-    postal_code: {
+    description: {
         type: String,
     },
 }, { timestamps: true });
