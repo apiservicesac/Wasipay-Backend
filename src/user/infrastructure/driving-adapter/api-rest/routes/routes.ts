d783a4a3fd079930f7969231e74d4bdbb9d6aee5
Josuesp1620 <josuesp1620@gmail.com>
2024-07-10 23:27:13 -0500

    Add Middleware Token User Routes

--- src/user/infrastructure/driving-adapter/api-rest/routes/routes.ts
+++ src/user/infrastructure/driving-adapter/api-rest/routes/routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import { ROUTES_USER } from '@/core/routes/v1/const.routes'
 import { loginController, createController, deleteController, getAllController, getByIdController, updateController, updateFieldController } from '../controllers'
+import { TokenMiddleware } from '@/shared/middleware/TokenMiddleware'
  
 const route = Router()
-
+const tokenMiddleware = new TokenMiddleware()
 route.post(ROUTES_USER.USER_LOGIN, loginController)
-route.post(ROUTES_USER.USER_CREATE, createController)
-route.delete(ROUTES_USER.USER_DELETE, deleteController)
-route.get(ROUTES_USER.USER_GET_ALL, getAllController)
-route.get(ROUTES_USER.USER_GET_BY_ID, getByIdController)
-route.put(ROUTES_USER.USER_UPDATE, updateController)
-route.patch(ROUTES_USER.USER_UPDATE_FIELD, updateFieldController)
+route.post(ROUTES_USER.USER_CREATE, tokenMiddleware.checkToken, createController)
+route.delete(ROUTES_USER.USER_DELETE, tokenMiddleware.checkToken, deleteController)
+route.get(ROUTES_USER.USER_GET_ALL, tokenMiddleware.checkToken, getAllController)
+route.get(ROUTES_USER.USER_GET_BY_ID, tokenMiddleware.checkToken, getByIdController)
+route.put(ROUTES_USER.USER_UPDATE, tokenMiddleware.checkToken, updateController)
+route.patch(ROUTES_USER.USER_UPDATE_FIELD, tokenMiddleware.checkToken, updateFieldController)
 
 
 export default route
