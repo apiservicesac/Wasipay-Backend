eb0d2994254d42a6c956db22ec9ae90c64cb0b20
Josuesp1620 <josuesp1620@gmail.com>
2024-07-11 10:38:54 -0500

    Update Address Mappers

--- src/address/domain/mappers/index.ts
+++ src/address/domain/mappers/index.ts
@@ -5,6 +5,10 @@ export class AddressDtoMapper {
     static toJson(address: AddressEntity): any {
         return {
             id: address._id,
+            first_name: address.first_name,
+            last_name: address.last_name,
+            phone_number: address.phone_number,
+            email: address.email,
             customer_id: address.customer_id,
             street: address.street,
             city: address.city,
