e58b181caa0fde70e1dec17fe2ed5d4cb315b99a
Josuesp1620 <josuesp1620@gmail.com>
2024-07-17 11:38:21 -0500

    [Update] Add Image Upload PaymentMethodsShop

--- src/payment_method_shop/infrastructure/driving-adapter/api-rest/controllers/UpdateController.ts
+++ src/payment_method_shop/infrastructure/driving-adapter/api-rest/controllers/UpdateController.ts
@@ -4,10 +4,29 @@ import { UpdateUseCase as UseCase } from '@/payment_method/application/use_cases
 import { ImplementationMongoose } from '@/payment_method/infrastructure/implementation/mongoose';
 import { PaymentMethodEntity } from '@/payment_method/domain/entities';
 
+import { ImplementationMongoose as ImplementationImageUploader } from '@/image_uploader/infrastructure/implementation/mongoose';
+import { RemoveImageUseCase, SaveImageUseCase } from '@/image_uploader/application/use_case';
+import { CreateEntityException } from '@/shared/exceptions';
+
+
 export const updateController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+        const images = req?.files?.images;
+
         const entityId = req.params.id;        
-        const body_data = req.body;
+        let body_data = req.body;
+
+        if (images && body_data.image_delete_id) {
+            const mongooseImageUploaderRepository = new ImplementationImageUploader()
+            // Delete Images
+            const deleteImageUseCase = new RemoveImageUseCase(mongooseImageUploaderRepository)
+            await deleteImageUseCase.run([body_data.image_delete_id])
+
+            const saveImageUseCase = new SaveImageUseCase(mongooseImageUploaderRepository)
+            const images_entities = await saveImageUseCase.run(body_data.shop_id, 'shop_payments', null, images)
+            if (images_entities.length === 0) throw new CreateEntityException()
+            body_data = {...body_data, image: images_entities[0]._id}
+        }
 
         const sequelizeRepository = new ImplementationMongoose()
         const useCase = new UseCase(sequelizeRepository)
