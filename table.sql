8ef492e9cab5f58bcd588de2e8de781aa928c023
Josuesp1620 <josuesp1620@gmail.com>
2024-09-03 19:29:29 -0500

    [Update] SQL File Product

--- table.sql
+++ table.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE wasipay;
+DROP DATABASE wasipay;
+
+
+DROP TABLE product;
 
 CREATE OR REPLACE FUNCTION correct_image_url()
 RETURNS TRIGGER AS $$
@@ -13,8 +16,49 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trigger_correct_image_url
 BEFORE INSERT OR UPDATE ON product
-FOR EACH ROW
-EXECUTE FUNCTION correct_image_url();
+FOR EACH ROW EXECUTE FUNCTION correct_image_url();
+
+CREATE OR REPLACE FUNCTION set_default_if_empty()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Manejar el campo discount
+    IF NEW.discount IS NULL OR TRIM(NEW.discount) = '' THEN
+        NEW.discount := '0';
+    END IF;
+
+    -- Manejar el campo price
+    IF NEW.price IS NULL OR TRIM(NEW.price) = '' THEN
+        NEW.price := '0';
+    ELSE
+        NEW.price := REPLACE(NEW.price, ',', '');
+    END IF;
+
+    -- Manejar el campo price_card
+    IF NEW.price_card IS NULL OR TRIM(NEW.price_card) = '' THEN
+        NEW.price_card := '0';
+    ELSE
+        NEW.price_card := REPLACE(NEW.price_card, ',', '');
+    END IF;
+
+    -- Manejar el campo price_offer
+    IF NEW.price_offer IS NULL OR TRIM(NEW.price_offer) = '' THEN
+        NEW.price_offer := '0';
+    ELSE
+        NEW.price_offer := REPLACE(NEW.price_offer, ',', '');
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_set_default_if_empty ON product;
+
+CREATE TRIGGER trigger_set_default_if_empty
+BEFORE INSERT OR UPDATE ON product
+FOR EACH ROW EXECUTE FUNCTION set_default_if_empty();
+
+
+
 
 -- Instalar la extensión uuid-ossp si no está instalada
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -37,4 +81,5 @@ EXECUTE FUNCTION set_uuid_before_insert();
 
 DELETE FROM ripley;
 SELECT * FROM product order by id desc;
+SELECT * FROM product where id = 'd83b9508-3e9e-4cac-9d5b-50b76e9d2dc7';
 SELECT COUNT(*) FROM ripley order by id desc;
